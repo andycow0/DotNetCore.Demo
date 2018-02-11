51269714ff777b954cdbf97633cfbc934263a8e6
andycow0 <andycow0@gmail.com>
2018-02-11 10:13:42 +0800

    Initialize completed.

--- Demo.BusinessLayer/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Demo.BusinessLayer
-{
-    public class Class1
-    {
-    }
-}
---
+++ Demo.BusinessLayer/Context/DbContexts/NorthwindDbContext.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.BuissinessLayer
+{
+    public class NorthwindDbcontext : DbContext
+    {
+        public NorthwindDbcontext(DbContextOptions<NorthwindDbcontext> options) : base(options)
+        {
+            
+        }
+    }
+}

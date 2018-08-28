3cb348e53f80085cd2d0e526f3cdd08c4f7f6f02
Andycow0 <andycow0@gmail.com>
2018-08-29 00:49:38 +0800

    Httpheader with Middleware

--- Demo.WebService/Core/MiddleWares/RequestLogMiddleware.cs
+++ Demo.WebService/Core/MiddleWares/RequestLogMiddleware.cs
@@ -2,29 +2,43 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
-namespace Demo.WebService.Core.Middlewares {
-    public class RequestLogMiddleware {
+namespace Demo.WebService.Core.Middlewares
+{
+    public class RequestLogMiddleware
+    {
         private readonly RequestDelegate _next;
-        public RequestLogMiddleware (RequestDelegate next) {
+        public RequestLogMiddleware(RequestDelegate next)
+        {
             _next = next;
         }
 
-        public async Task Invoke (HttpContext context) {
+        public async Task Invoke(HttpContext context)
+        {
 
-            Console.WriteLine ("RequestLog test !");
+            Console.WriteLine("RequestLog test !");
             
-            await _next.Invoke (context);
+            // context.Request.Headers.Add("test-header","test1234");
+            context.Response.OnStarting(state =>
+            {
+                var httpContext = (HttpContext)state;
+                httpContext.Response.Headers.Add("X-Response-Time-Milliseconds", new[] { "test1234" });
+                return Task.FromResult(0);
+            }, context);
+
+            await _next.Invoke(context);
             // string response = GenerateResponse (context);
 
             // context.Response.ContentType = GetContentType ();
             // await context.Response.WriteAsync (response);
         }
-        private string GenerateResponse (HttpContext context) {
+        private string GenerateResponse(HttpContext context)
+        {
             string title = context.Request.Query["title"];
-            return string.Format ("Title of the report: {0}", title);
+            return string.Format("Title of the report: {0}", title);
         }
 
-        private string GetContentType () {
+        private string GetContentType()
+        {
             return "text/plain";
         }
     }

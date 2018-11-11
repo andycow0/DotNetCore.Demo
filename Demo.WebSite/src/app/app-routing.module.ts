eeaa8e91b6afacf2f388e4723f29c647e3336357
Andycow0 <andycow0@gmail.com>
2018-11-11 23:34:31 +0800

    Apply app.module as path setting.

--- Demo.WebSite/src/app/app-routing.module.ts
+++ Demo.WebSite/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListEmployeesComponent } from './order/list-employees.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'order/list', component: ListEmployeesComponent }
+];
 
 @NgModule({
   imports: [
+    RouterModule.forRoot(routes)
   ],
-  exports: [RouterModule],
-  declarations: []
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Demo.WebSite/src/app/app.module.ts
+++ Demo.WebSite/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+// import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ListEmployeesComponent } from './order/list-employees.component'
+import { ListEmployeesComponent } from './order/list-employees.component';
 
 import { MatButtonModule, MatIconModule } from '@angular/material'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { AppRoutingModule } from './/app-routing.module';
-
-const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'order/list', component: ListEmployeesComponent }
-];
-
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -24,13 +18,12 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    //AppRoutingModule
+    AppRoutingModule
   ],
-  exports: [RouterModule],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
   // entryComponents: [AppComponent]

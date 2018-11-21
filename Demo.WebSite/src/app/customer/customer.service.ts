138c77b4375fb404c7b3070b0c200db892dca4a1
Andycow0 <andycow0@gmail.com>
2018-11-21 10:56:56 +0800

    Customer Filter

--- Demo.WebSite/src/app/customer/customer.service.ts
+++ Demo.WebSite/src/app/customer/customer.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ICustomerService } from './icustomer.interface';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators'
 import { Customer } from '../model/customer.model';
 
 @Injectable({
@@ -14,8 +15,11 @@ export class CustomerService implements ICustomerService {
 
   delete(customerId: string): void {
     console.log('CustomerService delete Customer start!');
-    
-    this.httpClient.delete('http://localhost:5000/api/Customers/' + customerId);
+
+    this.httpClient.delete('http://localhost:5000/api/Customers/9000').pipe(
+      tap(_ => console.log('deleted Customer id:.')),
+      catchError(this.handleError<any>('delete Customer'))
+    );;
   }
 
   getAllCustomers(): Observable<Customer[]> {
@@ -23,5 +27,19 @@ export class CustomerService implements ICustomerService {
     // return this.httpClient.get<Customer[]>('https://dotnetcoredemo-217516.appspot.com/api/Customers');
 
     return this.httpClient.get<Customer[]>('http://localhost:5000/api/Customers/GetAllCustomers');
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
--- Demo.WebSite/src/app/customer/list-customer/list-customer.component.ts
+++ Demo.WebSite/src/app/customer/list-customer/list-customer.component.ts
@@ -12,12 +12,17 @@ export class ListCustomerComponent implements OnInit {
   ngOnInit(): void {
     this.getAllCustomers();
     //this.getAllString();
+    //this.customerId = '';
   }
 
   customers: Customer[];
   values: string[];
+  customer: Customer;
+  customerId: number;
+  show: boolean;
 
   constructor(private customerService: ICustomerService) {
+
   }
 
   private getAllCustomers() {
@@ -30,6 +35,46 @@ export class ListCustomerComponent implements OnInit {
         error => console.log(error),
         () => console.log('getAllCustomers() GET completed!')
       );
+  }
+
+  find(): void {
+    if (Number(this.customerId) > 0 || !this.isEmpty(this.customerId)) {
+      console.log('starting find customerId:' + this.customerId.toString());
+      // this.postService.find(this.postId, this.post);
+      this.customerService.getAllCustomers()
+        .subscribe(
+          (response) => {
+            let result = response;
+            //console.log(result.length);
+            console.log(result);
+            var id = this.pad(this.customerId, 5);
+            console.log(id);
+            if (result.length > 0)
+              this.customers = result.filter(p => p.customerId == id);
+            else
+              this.customers = result;
+            console.log(this.customers.length);
+            if (this.customers.length > 0) {
+              this.customer = this.customers[0];
+              this.show = true;
+            }
+            else { this.show = false; }
+
+          }
+        );
+    }
+    else {
+      this.getAllCustomers();
+      this.show = false;
+    }
+  }
+  isEmpty(input) {
+    return (input.replace(/\s/g, "").length > 0 ? false : true);
+  }
+  pad(num: number, size: number): string {
+    let s = num + "";
+    while (s.length < size) s += " ";
+    return s;
   }
 
   delete(customerId: string) {
